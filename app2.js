f19d34553ba9b5c145ed8e1835673634c12508b7
KC Hofstetter <kclynhofstetter@gmail.com>
2022-04-25 11:23:50 -0700

    added javascript file for user input if they want to see the hidden garden, added hidden message at the bottom of the webpage

---
+++ app2.js
@@ -0,0 +1,5 @@
+let userAnswer = prompt('Do you want to discover the hidden garden (yes or no)')
+
+if (userAnswer.toLowerCase() == "yes"){
+    document.write('~*~*~*~*~*~*~*~*~*~*~Welcome to the hidden garden*~*~*~*~*~*~*~*~*~*~')
+}
