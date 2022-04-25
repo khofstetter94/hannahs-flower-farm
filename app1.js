59d2583e4863803521b43061da72006c5b46e2c8
KC Hofstetter <kclynhofstetter@gmail.com>
2022-04-25 11:22:57 -0700

    created javascript for prompt and user input of their name for welcome text

---
+++ app1.js
@@ -0,0 +1,14 @@
+// console.log('Hello World');
+
+// let userName = 'KC';
+// console.log(userName);
+
+let userName = prompt('Please Enter Your Name');
+
+console.log(userName);
+
+if(userName == 'Hannah'){
+    document.write('Welcome to your garden ' + userName)
+} else {
+    document.write("Welcome " + userName)
+}
