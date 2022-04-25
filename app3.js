c0aed236a0fe3e1c17a71423aee3792b61ac1c59
KC Hofstetter <kclynhofstetter@gmail.com>
2022-04-25 11:27:40 -0700

    added extra question mark to question

--- app3.js
+++ app3.js
@@ -1,4 +1,4 @@
-let anotherPicture = prompt("Do you want to see our garden cat?");
+let anotherPicture = prompt("Do you want to see our garden cat??");
 let picURL = "https://pismobeachvet.com/wp-content/uploads/2019/04/CatGarden_feature.jpg"
 
 if (anotherPicture.toLowerCase() == "yes"){
