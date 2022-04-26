5d1d08a9d5f91d9ec593ca70abe6a7ca73e3fe09
KC Hofstetter <kclynhofstetter@gmail.com>
2022-04-26 13:40:59 -0700

    added guessing game, added user input to add flowers to the secret garden

--- app2.js
+++ app2.js
@@ -29,7 +29,7 @@ function askUserQuestion(){
 }
 
 function anotherPictureQuestion(){
-    let anotherPicture = prompt("Do you want to see our garden cat??");
+    let anotherPicture = prompt("Do you want to see our garden cat?? (yes or no)");
     let picURL = "https://pismobeachvet.com/wp-content/uploads/2019/04/CatGarden_feature.jpg"
 
     if (anotherPicture.toLowerCase() == "yes"){
@@ -39,3 +39,40 @@ function anotherPictureQuestion(){
     }
 }
 
+function guessingGame(){
+    let userAnswer = prompt('Please Enter a Number 1-50');
+    let correctAnswer = 20;
+    let attempts = 8;
+
+    for(let i = 1; i < attempts; i++){
+        while(userAnswer < 1 || userAnswer > 100){
+            userAnswer = prompt('Please Try Again! A number 1-50')
+        }
+        if (userAnswer == correctAnswer){
+            alert('Correct! You are really good at guessing!');
+            document.write("Winner, winner, chicken dinner!");
+            break;
+        } else if (userAnswer > correctAnswer){
+            userAnswer = prompt('Sorry, too high. Please Try Again! A number 1-50');
+        } else if (userAnswer < correctAnswer){
+            userAnswer = prompt('Sorry, too low. Please Try Again! A number 1-50');
+        }
+        if (i == 7){
+            alert('The correct answer was ' + correctAnswer);
+            document.write("Game loser");
+        }
+    }
+}
+
+function moreFlowers(){
+    let usersFlowers = prompt('How many flowers will you add to the secret garden? (1-10)');
+    let picFlowerURL = "http://www.clker.com/cliparts/o/q/B/b/a/p/purple-flower-th.png"
+
+    for (let i=0; i < usersFlowers; i++){
+        document.write('<img src="' + picFlowerURL + '">');
+
+        if (usersFlowers < 1 || usersFlowers > 10){
+            usersFlowers = prompt('1-10 only please')
+        }
+    }
+}
